ed4e7fe8e566275c04fec7c2f2a5d8869bc63975
Hoangkhongvui <121917433+Hoangkhongvui@users.noreply.github.com>
2024-07-20 21:28:06 +0700

    thêm mới sản phẩm, đang làm dở phầm edit sản phẩm , thêm file Callpost.js ở public

--- apps/models/Post.js
+++ apps/models/Post.js
@@ -18,7 +18,54 @@ function getAllPost(){
             return defer.promise;
     
 };
-module.exports={
-    getAllPost:getAllPost
+function addPost(params){
+    if(params){
+        var defer = q.defer();
+        var query = conn.query('INSERT INTO posts SET ?',params,function(err,result){
+                if(err){
+                    defer.reject(err);
+                }
+                else{
+                    defer.resolve(result);
+                }
+                
+        });
+        return defer.promise;
+
+    }
+    return false;
 }
+function getPostByID(id){
+    var defer = q.defer();
+            var query = conn.query('SELECT * FROM posts where ? ',{id:id}, function(err,posts){
+                    if(err){
+                        defer.reject(err);
+                    }
+                    else{
+                        defer.resolve(posts);
+                    }
+            });
+            return defer.promise;
+};
+function updatePost(params){
+    if(params){
+        var defer = q.defer();
+            var query = conn.query(' UPDATE posts SET title= ?,content=?,author=?,img=?,updatedAt=? where id=? ',[params.id,params.title,params.content,params.author,params.img,new Date()], function(err,result){
+                    if(err){
+                        defer.reject(err);
+                    }
+                    else{
+                        defer.resolve(result);
+                    }
+            });
+            return defer.promise;
+    }
+    return false;
+}
+module.exports={
+    getAllPost:getAllPost,
+    addPost:addPost,
+    getPostByID:getPostByID,
+    updatePost:updatePost
+};
 
